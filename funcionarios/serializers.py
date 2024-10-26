6a42d196dae047138c0d48937a06ab9bd5cbaef2
WalbeRodri <walber.rodri@gmail.com>
2024-10-26 09:38:39 -0300

    initial commit

---
+++ funcionarios/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Funcionario
+
+class FuncionarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Funcionario
+        fields = ('nome', 'cpf', 'email', 'cargo', 'meta', 'data_cadastro')

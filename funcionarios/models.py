7377ff04a3fd4ac6643a168a5a34006ba7971416
Julia Pontes <juliaisap@gmail.com>
2025-12-09 19:42:22 -0300

    Adicionando model de exemplo

--- funcionarios/models.py
+++ funcionarios/models.py
@@ -27,3 +27,9 @@ class Cliente(Pessoa):
         verbose_name_plural = 'clientes'
     def __str__(self):
         return self.endereco
+
+class Walber(Pessoa):
+    saldo = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name} = {self.saldo}'

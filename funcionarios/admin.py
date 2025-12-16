99cc6ddedbf4fb6aecf748404e3002fb9e2f7155
kassiothomaz <k.thomaz@live.com>
2025-12-16 20:01:09 -0300

    Kassio's alteration

--- funcionarios/admin.py
+++ funcionarios/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Funcionario,Kassiooo
 # Register your models here.
 
+
 @admin.register(Funcionario)
 class FuncionarioAdmin(admin.ModelAdmin):
     list_display = ('nome','cpf', 'meta', 'cargo','email', 'data_cadastro')

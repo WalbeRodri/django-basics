18b411e20c7fa0ec014cfae4689955d5e83afa4d
Eduardopmc <eduardopmc@gmail.com>
2025-12-16 20:11:53 -0300

    Registro Admin Rayanne

--- funcionarios/admin.py
+++ funcionarios/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Funcionario
+from .models import Funcionario, Rayanne
 # Register your models here.
 
 @admin.register(Funcionario)
 class FuncionarioAdmin(admin.ModelAdmin):
     list_display = ('nome','cpf', 'meta', 'cargo','email', 'data_cadastro')
+
+@admin.register(Rayanne)
+class RayanneAdmin(admin.ModelAdmin):
+    list_display = ('nacionalidade', 'altura', 'cor_do_cabelo', 'hobbies')

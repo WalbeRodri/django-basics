2f73b115f3de6e5fa20f1e029d30e2217bb414e9
Domii00 <domii.ju00@gmail.com>
2025-12-16 19:59:36 -0300

    Refactor admin.py to include  Dominique models in the admin interface

--- funcionarios/admin.py
+++ funcionarios/admin.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
-from .models import Funcionario
+from .models import Funcionario, Eduardo, Dominique
 # Register your models here.
 
 @admin.register(Funcionario)
 class FuncionarioAdmin(admin.ModelAdmin):
     list_display = ('nome','cpf', 'meta', 'cargo','email', 'data_cadastro')
-
-@admin.register(Eduardo)
-class Eduardo(Pessoa):
-    list_display = ('caracteristicas')
+  
     
-    
-   
+@admin.register(Dominique)
+class DominiqueAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'idade', 'cor')
